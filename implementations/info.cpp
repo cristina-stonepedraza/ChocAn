59cb31a01e0c62f2979086a55f5606bb4c372fe4
dmotorca <dmotorca@pdx.edu>
2021-10-25 11:24:34 -0700

    Update info.cpp
    I added a branch and am just messing around with things to get familiar

--- implementations/info.cpp
+++ implementations/info.cpp
@@ -5,6 +5,7 @@ using namespace std;
 personalInfo::personalInfo(){
     //
     cout<<"Wow this was not easy to get setup I had some huge issues";
+    cout<<"testing second editor";
 }
 
 personalInfo::~personalInfo(){
